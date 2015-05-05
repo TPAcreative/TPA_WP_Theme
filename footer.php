a844a8eed25f7cad1e951bec8a17f71b6ca71960
Chris Mousdale <chris@mousdale.com>
2015-05-05 17:09:18 +0100

    Better IE Specificity

--- footer.php
+++ footer.php
@@ -1,15 +1,17 @@
-	<footer class="main-footer">
+<footer class="main-footer">
 
-	</footer>
+</footer>
 
 <?php wp_footer(); ?>
 
-<!--[if IE]>
-    <script src="<?php bloginfo('template_directory'); ?>/js/jquery.placeholder.min.js"></script>
-    <script>
-        jQuery('input[placeholder]').placeholder();
-    </script>
-    <script src="<?php bloginfo('template_directory'); ?>/js/respond.min.js"></script>
+<!--[if lte IE9]>
+	<script src="<?php bloginfo('template_directory'); ?>/js/jquery.placeholder.min.js"></script>
+	<script>
+			jQuery('input[placeholder]').placeholder();
+	</script>
+<![endif]-->
+<!--[if lt IE9]>
+	<script src="<?php bloginfo('template_directory'); ?>/js/respond.min.js"></script>
 <![endif]-->
 
 <!-- GOOGLE ANALYTICS -->
