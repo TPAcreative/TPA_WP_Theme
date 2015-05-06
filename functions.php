9d1a80510625621be81ef2f89f19e6ee11619d70
Chris Mousdale <chris@mousdale.com>
2015-05-06 16:56:36 +0100

    functions: clean up

--- functions.php
+++ functions.php
@@ -129,38 +129,14 @@ include_once('includes/helpers.php');
 include_once('includes/custom-image-sizes.php');
 
 /*---------------------------------------------------------
-    [ Clean up Menus ]
+    [ Fixes ]
 ----------------------------------------------------------*/
-add_filter('nav_menu_css_class', 'my_css_attributes_filter', 100, 1);
-add_filter('nav_menu_item_id', 'my_css_attributes_filter', 100, 1);
-add_filter('page_css_class', 'my_css_attributes_filter', 100, 1);
-function my_css_attributes_filter($var) {
-  return is_array($var) ? array_intersect($var, array('current-menu-item')) : '';
-}
+// include_once('includes/fixes/child-nav.php');
 
 /*---------------------------------------------------------
-    [ Bump Header ]
+    [ Tidy up Wordpress output ]
 ----------------------------------------------------------*/
-// add_action('get_header', 'my_filter_head');
-
-//   function my_filter_head() {
-//     remove_action('wp_head', '_admin_bar_bump_cb');
-//   }
-
-
-/*---------------------------------------------------------
-    [ Clean Header ]
-----------------------------------------------------------*/
-remove_action( 'wp_head', 'feed_links_extra', 3 ); // Display the links to the extra feeds such as category feeds
-remove_action( 'wp_head', 'feed_links', 2 ); // Display the links to the general feeds: Post and Comment Feed
-remove_action( 'wp_head', 'rsd_link' ); // Display the link to the Really Simple Discovery service endpoint, EditURI link
-remove_action( 'wp_head', 'wlwmanifest_link' ); // Display the link to the Windows Live Writer manifest file.
-remove_action( 'wp_head', 'index_rel_link' ); // index link
-remove_action( 'wp_head', 'parent_post_rel_link', 10, 0 ); // prev link
-remove_action( 'wp_head', 'start_post_rel_link', 10, 0 ); // start link
-remove_action( 'wp_head', 'adjacent_posts_rel_link', 10, 0 ); // Display relational links for the posts adjacent to the current post.
-remove_action( 'wp_head', 'wp_generator' ); // Display the XHTML generator that is generated on the wp_head hook, WP version
-
+include_once('includes/clean-up.php');
 
 /*---------------------------------------------------------
     [ Widgets ]
---
+++ includes/clean-up.php
@@ -0,0 +1,26 @@
+<?php
+
+/*---------------------------------------------------------
+    [ Clean up Menus ]
+----------------------------------------------------------*/
+add_filter('nav_menu_css_class', 'my_css_attributes_filter', 100, 1);
+add_filter('nav_menu_item_id', 'my_css_attributes_filter', 100, 1);
+add_filter('page_css_class', 'my_css_attributes_filter', 100, 1);
+function my_css_attributes_filter($var) {
+  return is_array($var) ? array_intersect($var, array('current-menu-item')) : '';
+}
+
+/*---------------------------------------------------------
+    [ Clean Header ]
+----------------------------------------------------------*/
+remove_action( 'wp_head', 'feed_links_extra', 3 ); // Display the links to the extra feeds such as category feeds
+remove_action( 'wp_head', 'feed_links', 2 ); // Display the links to the general feeds: Post and Comment Feed
+remove_action( 'wp_head', 'rsd_link' ); // Display the link to the Really Simple Discovery service endpoint, EditURI link
+remove_action( 'wp_head', 'wlwmanifest_link' ); // Display the link to the Windows Live Writer manifest file.
+remove_action( 'wp_head', 'index_rel_link' ); // index link
+remove_action( 'wp_head', 'parent_post_rel_link', 10, 0 ); // prev link
+remove_action( 'wp_head', 'start_post_rel_link', 10, 0 ); // start link
+remove_action( 'wp_head', 'adjacent_posts_rel_link', 10, 0 ); // Display relational links for the posts adjacent to the current post.
+remove_action( 'wp_head', 'wp_generator' ); // Display the XHTML generator that is generated on the wp_head hook, WP version
+
+?>
